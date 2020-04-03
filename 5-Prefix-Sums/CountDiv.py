1003d90676b72c45e7521a59e5f016ee182e6da0
Tarsio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-04-03 17:56:58 -0300

    Terminei o desafio 5 com uma ajuda online, percebi q precisava também ajustar a argumento 'a' para o primeiro valor possível, coisa q já fazia em outra proposta desse problema, adicionei isso e também ajustei o último valor. Também adicionei testes. Será o padrão a partir de agora, primeiroe screvo os testes.

--- 5-Prefix-Sums/CountDiv.py
+++ 5-Prefix-Sums/CountDiv.py
@@ -1,6 +1,6 @@
 from functools import reduce
-from math import floor
-
+from math import floor, ceil
+import random
 
 def solution(a, b, k):
     return len(list(filter(lambda x: (x % k) == 0, range(a, b + 1))))
@@ -35,21 +35,64 @@ def solution(a, b, k):
     return reduce(lambda s, x: s + 1, range(a1, b + 1, k), 0)
 
 
-def solution(a, b, d):
+def solution(a, b, k):
     if b == a:
-        if b % d == 0:
+        if b % k == 0:
             return 1
         else:
             return 0
-    n = (b - a) / d + 1
+    if k == 0:
+        return 0
+    if a > b:
+        return -1
+    a1 = ceil(a/k) * k
+    b1 = floor(b/k) * k
+    n = (b1 - a1) / k + 1
     return floor(n)
 
 
+def test(f, a, b, k):
+    ra = random.randint(a[0], a[1])
+    rb = random.randint(ra, b[1])
+    rk = random.randint(k[0], k[1])
+    s = f(ra, rb, rk)
+    return ra, rb, rk, s
+
+
+def main():
+    a = 6
+    b = 11
+    k = 2
+    t = solution(a, b, k)
+    print(a, b, k, t)
+
+    ca = [0, 20]
+    cb = [0, 20]
+    ck = [0, 20]
+    for r in range(10):
+        t = test(solution, ca, cb, ck)
+        print("%d\t%d\t%d\t%d" % (t[0], t[1], t[2], t[3]))
+
+    pa = [0, 2000000000]
+    pb = [0, 2000000000]
+    pk = [0, 2000000000]
+    for r in range(10):
+        t = test(solution, pa, pb, pk)
+        print("%d\t%d\t%d\t%d" % (t[0], t[1], t[2], t[3]))
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
 A = 11
 B = 13
 K = 2
 print(solution(A, B, K))
-
+a = A
+b = B
+k = K
 
 A = 6
 B = 11
@@ -92,5 +135,5 @@ A = 0
 B = 2 * 1000000000
 K = 1
 print(solution(A, B, K))
-
+"""
 
