576cc6974522e12e5f84dbad27e628a10421c1a4
Tarsio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-04-05 13:49:09 -0300

    Finish more two lessons.

---
+++ 6-Sorting/Distinct.py
@@ -0,0 +1,5 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(a):
+    return len(set(a))
---
+++ 7-Stacks-and-Queues/Brackets.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(string):
+    if len(string) == 0:
+        return 1
+    d = {"{": 0, "[": 1, "(": 2, "}": 3, "]": 4, ")": 5}
+    stack = deque()
+    stack.append(string[0])
+    for s in string[1:]:
+        n = d[s]
+        if len(stack) == 0:
+            stack.append(s)
+            continue
+        last = d[stack[-1]]
+        if n == last + 3:
+            stack.pop()
+        else:
+            stack.append(s)
+    s = len(stack) == 0
+    return int(s)
+
+
+string = "([)()]"
+s = solution(string)
+print(s, string)
+string = "{[()()]}"
+s = solution(string)
+print(s, string)
+
+
+
